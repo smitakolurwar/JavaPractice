eda4fe8f2a61b1a83d46983e4891271867981156
Smita Kolurwar <smitakolurwar@gmail.com>
2022-07-07 15:35:44 -0400

    reverse string code

---
+++ src/main/java/StringPrograms/duplicateElement.java
@@ -0,0 +1,19 @@
+package StringPrograms;
+
+
+
+import javax.swing.*;
+import java.sql.ClientInfoStatus;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class duplicateElement {
+
+    public static void main(String[] args) {
+
+        final List <Integer> str = Arrays.asList(10,20,35,10,35,89,63,20);
+        Set<String> set = new HashSet<>();
+
+
+    }
+}

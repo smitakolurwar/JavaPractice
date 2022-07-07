80cf8ac0a59d474ba65dad1b58594f256ac08f69
Smita Kolurwar <smitakolurwar@gmail.com>
2022-07-07 15:42:46 -0400

    reverse string code

--- src/main/java/StringPrograms/ReverseString.java
+++ src/main/java/StringPrograms/ReverseString.java
@@ -3,15 +3,12 @@ package StringPrograms;
 public class ReverseString {
 
     public static void main(String[] args) {
-         String str = "My name is smita ";
-        System.out.println(reverseWtihStringBuilder(str));
-
+         String str = "My name is smita";
+        System.out.println(reverseWithStringBuilder(str));
     }
 
-
-    private static String reverseWtihStringBuilder(String str){
+    private static String reverseWithStringBuilder(String str){
         return new StringBuilder(str).reverse().toString();
-
     }
 }
 
